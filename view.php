3e78ab11c255a3686d41f302692f444284d87fb5
NITISH <89100185+NitishR24@users.noreply.github.com>
2022-01-05 00:03:11 +0530

    Add files via upload

--- view.php
+++ view.php
@@ -12,7 +12,41 @@
         <body>
         	<div class="type" data-splitting>THANK YOU FOR REGISTERING!!!<br><br>___YOUR ID IS___</div>
 
+		<?php
+		include 'C:\xampp\htdocs\myprog\demo\config.php';
 		
+	$cust_name = $_POST['cust_name'];
+	$cust_phone = $_POST['cust_phone'];
+	$password = $_POST['password'];
+
+    $sql = "INSERT INTO cust (cust_name,cust_phone,password) VALUES ('$cust_name','$cust_phone','$password')";
+
+    $result = $conn->query($sql);
+
+    if($result == TRUE) { ?>
+	<script> alert("New record created succesfully") </script>
+    <?php 
+	}
+    else {
+	echo "Error:" . $sql . "<br>" .$conn->error;
+	}
+	
+	
+		$sql2 = "SELECT * FROM cust ";
+		$result2 = $conn->query($sql2);
+		
+		while($rows = mysqli_fetch_array($result2))
+        {
+         $id = $rows['cust_id'];
+		 $cust_nameid = $rows['cust_name'];
+	     $cust_phoneid = $rows['cust_phone'];
+	     $passwordid = $rows['password'];
+        }
+		?>
+		<p><?php echo $id; ?></p> <br>
+		<p><?php echo $cust_nameid; ?></p> <br>
+		<p><?php echo $cust_phoneid; ?></p> <br>
+		<p><?php echo $passwordid; ?></p>
 		<script src="C:\xampp\htdocs\myprog\demo\splitting.js"></script>
 		<script> Splitting(); </script>
 </body>
